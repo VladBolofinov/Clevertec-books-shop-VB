5df2efd4c6888b3a9ff9ebe8ebe2d5aba90f046e
Vlad Bolofinov <vlad.bolofinov@gmail.com>
2023-11-05 21:28:19 +0300

    refactor code

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,28 +1,28 @@
 import {RouteProps} from "react-router-dom";
-import MainPage from "../../../pages/MainPage/MainPage";
+//import MainPage from "../../../pages/MainPage/MainPage";
 import RulesPage from "../../../pages/RulesPage/RulesPage";
 import ContractOfferPage from "../../../pages/ContractOfferPage/ContractOfferPage";
 import BookPage from "../../../pages/BookPage/BookPage";
 
 export enum AppRoutes {
-    MAIN ='main',
+    //MAIN ='main',
     RULES = 'rules',
     CONTRACT = 'contractOffer',
     BOOKID = 'bookPage',
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
-    [AppRoutes.MAIN]: '/',
+    //[AppRoutes.MAIN]: '/',
     [AppRoutes.RULES]: '/rules',
     [AppRoutes.CONTRACT]: '/contract_offer',
     [AppRoutes.BOOKID]: '/bookPage/'
 }
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
-    [AppRoutes.MAIN]: {
+    /*[AppRoutes.MAIN]: {
         path: RoutePath.main,
         element: <MainPage/>
-    },
+    },*/
     [AppRoutes.RULES]: {
         path: RoutePath.rules,
         element: <RulesPage/>
