7550ad0c7ca04407996fd814f7f5ea9c88f1482b
Vlad Bolofinov <vlad.bolofinov@gmail.com>
2023-12-20 21:02:55 +0300

    made registration step 1

--- src/components/authPage/registration/Registration.tsx
+++ src/components/authPage/registration/Registration.tsx
@@ -21,11 +21,15 @@ export const Registration = () => {
         handleSubmit,
         reset,
         formState: { errors },
+        getValues,
     } = useForm<Inputs>({
         mode: 'onChange'
     })
+
     const onSubmit: SubmitHandler<Inputs> = (data) => {
         console.log(data);
+        console.log(getValues('password'));
+        console.log(getValues('login'));
         reset();
     }
 
@@ -50,96 +54,98 @@ export const Registration = () => {
             }
         }
     }
-    const {isOnFocusLoginPlaceholder, isOnFocusPasswordPlaceholder, inputLoginValue, inputPasswordValue, inputType,
-        requestStatus, authSuccess} = useAppSelector(state => state.authorizationReducer);
-    const {setOnFocusPlaceholder, setInputLoginValue, setInputPasswordValue, setInputType, repeatLogIn} = authorizationSlice.actions;
+    const {isOnFocusLoginPlaceholder, isOnFocusPasswordPlaceholder, inputLoginValue, inputPasswordValue, inputType,} = useAppSelector(state => state.authorizationReducer);
+    const {setOnFocusPlaceholder, setInputLoginValue, setInputPasswordValue, setInputType} = authorizationSlice.actions;
     const dispatch = useAppDispatch();
     return (
             <>
             <div className={styles.modal}>
                 <p className={styles.enterAccount}>Регистрация</p>
+                <p className={styles.stepText}>1 шаг из 3</p>
                 <p className={(isOnFocusLoginPlaceholder || inputLoginValue) ? styles.onFocusPlaceholder : styles.notFocusPlaceholder}>Придумайте логин для входа</p>
-                <input className={(authSuccess) ? styles.loginInput : (requestStatus === 0) ? styles.loginInput : styles.loginInputWrong} type="text"
+                <form onSubmit={handleSubmit(onSubmit)}>
+                <input className={(errors.login?.message) ? styles.loginInputWrong  : styles.loginInput} type="text"
                        onFocus={() => dispatch(setOnFocusPlaceholder('Логин'))}
-                       onBlur={() => dispatch(setOnFocusPlaceholder('Логин'))}
-                       onChange={(e) => dispatch(setInputLoginValue(e.target.value))}/>
+                       {...register("login",
+                           {
+                               onBlur: () => dispatch(setOnFocusPlaceholder('Логин')),
+                               onChange:(e) => dispatch(setInputLoginValue(e.target.value)),
+                               required: true,
+                               validate: {
+                                   checkLogin: (value) => {
+                                       const hasSpecificSymbols = /[^a-zA-Z0-9]/g.test(value);
+                                       const hasNumbersOnly = /\d/.test(value);
+                                       const hasAllParams = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+$/.test(value);
+                                       if (hasAllParams) {
+                                           return true;
+                                       } else if (hasSpecificSymbols && hasNumbersOnly) {
+                                           return 'латинский алфавит';
+                                       } else {
+                                           return 'Используйте для логина латинский алфавит и цифры';
+                                       }
+                                   },
+                               },
+                           })}/>
+                <p className={styles.inputTooltip}>{(errors.login?.message)
+                    ? highlightedText('login',errors.login.message)
+                    : 'Используйте для логина латинский алфавит и цифры'}</p>
                 <div className={styles.wrapperInput}>
                     <p className={(isOnFocusPasswordPlaceholder || inputPasswordValue) ? styles.onFocusPlaceholder : styles.notFocusPlaceholder}>Пароль</p>
-                    <input className={(authSuccess) ? styles.passwordInput : (requestStatus === 0) ? styles.passwordInput : styles.passwordInputWrong} type={inputType}
+                    <input className={(errors.password?.message) ? styles.passwordInputWrong  : styles.passwordInput} type={inputType}
                            onFocus={() => dispatch(setOnFocusPlaceholder('Пароль'))}
-                           onBlur={() => dispatch(setOnFocusPlaceholder('Пароль'))}
-                           onChange={(e) => dispatch(setInputPasswordValue(e.target.value))}/>
+                           {...register("password",
+                               {
+                                   onBlur: () => dispatch(setOnFocusPlaceholder('Пароль')),
+                                   onChange:(e) => dispatch(setInputPasswordValue(e.target.value)),
+                                   required: true,
+                                   validate: {
+                                       checkPassword: (value) => {
+                                           const hasMinLength = /^.{8,}$/.test(value);
+                                           const hasUppercaseChar = /[A-Z]/.test(value);
+                                           const hasAllParams = /(?=.*[A-ZА-Яa-zа-я\d])(?=.*[A-ZА-Я]).{8,}/.test(value);
+                                           const hasNumbersOnly = /\d/.test(value);
+                                           if (hasAllParams) {
+                                               return true;
+                                           } else if (hasMinLength && hasNumbersOnly && !hasUppercaseChar) {
+                                               return 'с заглавной буквой';
+                                           } else if (hasNumbersOnly && !hasUppercaseChar && !hasMinLength) {
+                                               return 'не менее 8 символов';
+                                           } else {
+                                               return 'Пароль не менее 8 символов, с заглавной буквой и цифрой';
+                                           }
+                                       },
+                                   },
+                               })}/>
                     {(inputPasswordValue) ? <div className={styles.wrapperIcon}>
                         {(inputType === 'password') ? <IconEye onClick={() => dispatch(setInputType('text'))}/>
                             : <IconEyeClosed onClick={() => dispatch(setInputType('password'))}/>}
                     </div> : null}
                 </div>
-                {(authSuccess)
-                    ? <span className={styles.forgetEntryData}>Забыли логин или пароль?</span>
-                    : (requestStatus === 0)
-                        ? <span className={styles.forgetEntryData}>Забыли логин или пароль?</span>
-                        : <div className={styles.wrongAuthData}>
-                            <span className={styles.recoverData}>Неверный логин или пароль!</span>
-                            <span>Восстановить?</span>
-                        </div>}
-                <MyButton content={'Вход'} size={'btnLg'} onClick={() => dispatch(fetchLogIn({username:inputLoginValue,password:inputPasswordValue}))}/>
+                <p className={styles.inputTooltip}>{(errors.password?.message)
+                    ? highlightedText('password',errors.password.message)
+                    : 'Пароль не менее 8 символов, с заглавной буквой и цифрой'}</p>
+            </form>
+                <MyButton content={'Следующий шаг'}
+                          size={'btnLg'}
+                          margin={'25px 0 0 0'}
+                          onClick={() => {
+                              console.log(getValues('password'));
+                              console.log(getValues('login'));
+                              console.log(errors);
+                              // погугли в документации функцию/метод который дает
+                              // значение прошла ли валидация и нужно добавить в пасворд инпут иконку проверки
+                          }}/>
                 <div className={styles.registrationLink}>
-                    <span>Нет учетной записи?</span>
+                    <span>Есть учетная запись?</span>
                     <div className={styles.registrationLinkWrapper}>
-                        <NavLink to={`/authorization/registration`} style={() => ({
+                        <NavLink to={`/authorization`} style={() => ({
                             color: "black",
                             textDecoration: "red"
-                        })}><p>Регистрация</p></NavLink>
+                        })}><p>Войти</p></NavLink>
                         <IconArrowRight/>
                     </div>
                 </div>
             </div>
-            ////
-
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input {...register("login",
-                    {
-                        required: true,
-                        validate: {
-                            checkLogin: (value) => {
-                                const hasSpecificSymbols = /[^a-zA-Z0-9]/g.test(value);
-                                const hasNumbersOnly = /\d/.test(value);
-                                const hasAllParams = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+$/.test(value);
-                                if (hasAllParams) {
-                                    return true;
-                                } else if (hasSpecificSymbols && hasNumbersOnly) {
-                                    return 'латинский алфавит';
-                                } else {
-                                   return 'Используйте для логина латинский алфавит и цифры';
-                                }
-                            },
-                        },
-                    })} />
-                {(errors.login) && highlightedText('login',errors.login.message)}
-                <input {...register("password",
-                    {
-                        required: true,
-                        validate: {
-                            checkPassword: (value) => {
-                                const hasMinLength = /^.{8,}$/.test(value);
-                                const hasUppercaseChar = /[A-Z]/.test(value);
-                                const hasAllParams = /(?=.*[A-ZА-Яa-zа-я\d])(?=.*[A-ZА-Я]).{8,}/.test(value);
-                                const hasNumbersOnly = /\d/.test(value);
-                                if (hasAllParams) {
-                                    return true;
-                                } else if (hasMinLength && hasNumbersOnly && !hasUppercaseChar) {
-                                    return 'с заглавной буквой';
-                                } else if (hasNumbersOnly && !hasUppercaseChar && !hasMinLength) {
-                                    return 'не менее 8 символов';
-                                } else {
-                                   return 'Пароль не менее 8 символов, с заглавной буквой и цифрой';
-                                }
-                            },
-                        },
-                    })} />
-                {errors.password && highlightedText('password',errors.password.message)}
-                <input type="submit" />
-            </form>
         </>
     );
 };
