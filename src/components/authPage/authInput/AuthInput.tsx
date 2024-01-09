e583d83e0c332196b89b6a75be3ab47ee10f78bc
Vlad Bolofinov <vlad.bolofinov@gmail.com>
2024-01-10 00:32:09 +0300

    refactor registration logic with custom inputs

--- src/components/authPage/authInput/AuthInput.tsx
+++ src/components/authPage/authInput/AuthInput.tsx
@@ -2,27 +2,27 @@ import styles from './AuthInput.module.scss';
 import {useAppDispatch, useAppSelector} from "../../hooks/redux";
 import {authorizationSlice} from "../../../store/reducers/AuthorizationSlice";
 import {IRegistrationData} from "../../../store/stateTypes/IAuthorization";
-import {useForm} from "react-hook-form";
+import {DeepMap, FieldError, FieldValues, useForm, UseFormRegister} from "react-hook-form";
 import {highlightedText} from "../registration/highlightedTextFunc";
 import {checkLogin, checkPassword, ErrorMessageTooltip} from "../validationLogic";
 import IconCheck from "../../../assets/img/icons/AuthIcons/IconCheck.svg";
 import IconEye from "../../../assets/img/icons/AuthIcons/Eye.svg";
 import IconEyeClosed from "../../../assets/img/icons/AuthIcons/EyeClosed.svg";
-import React, {useRef} from "react";
 interface IAuthInputProps {
     needValidate?: boolean;
     inputRegistrationData?: keyof IRegistrationData;
     inputNumber?: 'First' | 'Second';
     inputTypeProp?: string;
     placeholderValue?: string;
+    register?: UseFormRegister<IRegistrationData>;
+    errors?: DeepMap<FieldValues, FieldError>;
 }
 
 
-export const AuthInput = ({needValidate, inputNumber, inputRegistrationData, inputTypeProp, placeholderValue}:IAuthInputProps) => {
-    const {isOnFocusFirstPlaceholder, isOnFocusSecondPlaceholder, inputType, registrationData, registrationStep} = useAppSelector(state => state.authorizationReducer);
+export const AuthInput = ({needValidate, inputNumber, inputRegistrationData, inputTypeProp, placeholderValue, register, errors}:IAuthInputProps) => {
+    const {isOnFocusFirstPlaceholder, isOnFocusSecondPlaceholder, inputType, registrationData} = useAppSelector(state => state.authorizationReducer);
     const {setOnFocusPlaceholder, setInputType, setRegistrationData} = authorizationSlice.actions;
     const dispatch = useAppDispatch();
-
     const validationRules = {
         ...(needValidate && {
             required: true,
@@ -34,40 +34,32 @@ export const AuthInput = ({needValidate, inputNumber, inputRegistrationData, inp
         }),
     };
 
-    const {
-        register,
-        handleSubmit,
-        reset,
-        formState: { errors, isValid },
-        getValues,
-    } = useForm<IRegistrationData>({
-        mode: 'onChange'
-    })
-
     return (
-        <>
-            <div className={styles.wrapperInput}>
-            <p className={(isOnFocusFirstPlaceholder || registrationData[inputRegistrationData]) ? styles.onFocusPlaceholder : styles.notFocusPlaceholder}>{placeholderValue}</p>
-            <input className={(needValidate) ? (errors.login?.message) ? styles.inputWrong  : styles.inputValid : styles.loginInput} type={inputType && inputTypeProp}
+            <div  className={styles.wrapperInput}>
+            <p className={(((inputNumber === 'First') ? isOnFocusFirstPlaceholder : isOnFocusSecondPlaceholder) || registrationData[inputRegistrationData]) ? styles.onFocusPlaceholder : styles.notFocusPlaceholder}>{placeholderValue}</p>
+            <input className={(needValidate) ? (errors.login?.message) ? styles.inputWrong  : styles.inputValid : styles.loginInput} type={inputTypeProp && inputType}
                    onFocus={() => dispatch(setOnFocusPlaceholder(inputNumber))}
                    {...register(inputRegistrationData, {
                        ...validationRules,
                        onBlur: () => dispatch(setOnFocusPlaceholder(inputNumber)),
                        onChange: (e) => {
+                           //console.log(isValid);
                            dispatch(setRegistrationData({ type: inputRegistrationData, value: e.target.value }));
-                       },
+                       }
                    })}/>
-
-                {(registrationData.password) ? <div className={styles.wrapperIcon}>
-                    {(errors.hasOwnProperty('password')) ? null : <IconCheck/>}
-                    {(inputType === 'password') ? <IconEye onClick={() => dispatch(setInputType('text'))}/>
-                        : <IconEyeClosed onClick={() => dispatch(setInputType('password'))}/>}
-                </div> : null}
-
-                <p className={styles.inputTooltip}>{(errors.login?.message)
-                    ? highlightedText(inputRegistrationData,errors.login.message)
+                {(registrationData.password && inputRegistrationData === 'password')
+                    ? <div className={styles.wrapperIcon}>
+                        {(errors.hasOwnProperty('password'))
+                            ? null
+                            : <IconCheck/>}
+                        {(inputType === 'password')
+                            ? <IconEye onClick={() => dispatch(setInputType('text'))}/>
+                            : <IconEyeClosed onClick={() => dispatch(setInputType('password'))}/>}
+                        </div>
+                    : null}
+                <p className={styles.inputTooltip}>{(errors[inputRegistrationData]?.message)
+                    ? highlightedText(inputRegistrationData,errors[inputRegistrationData].message)
                     : ErrorMessageTooltip[inputRegistrationData]}</p>
             </div>
-        </>
     );
 };
--- src/components/authPage/registration/Registration.tsx
+++ src/components/authPage/registration/Registration.tsx
@@ -1,40 +1,33 @@
-import React, {useRef} from 'react';
 import {useForm, SubmitHandler} from "react-hook-form";
 import styles from "./Registration.module.scss";
-import IconEye from "../../../assets/img/icons/AuthIcons/Eye.svg";
-import IconEyeClosed from "../../../assets/img/icons/AuthIcons/EyeClosed.svg";
-import IconCheck from "../../../assets/img/icons/AuthIcons/IconCheck.svg";
 import {MyButton} from "../../sharedComponents/MyButton/MyButton";
 import {authorizationSlice} from "../../../store/reducers/AuthorizationSlice";
 import {NavLink} from "react-router-dom";
 import IconArrowRight from "../../../assets/img/icons/AuthIcons/IconChevron.svg";
 import {useAppDispatch, useAppSelector} from "../../hooks/redux";
-import {highlightedText} from "./highlightedTextFunc";
 import {AuthInput} from "../authInput/AuthInput";
+import {IRegistrationData} from "../../../store/stateTypes/IAuthorization";
 type Inputs = {
     login: string;
     password: string
 }
 
 export const Registration = () => {
-    const {isOnFocusFirstPlaceholder, isOnFocusSecondPlaceholder, inputType, registrationData, registrationStep} = useAppSelector(state => state.authorizationReducer);
-    const {setOnFocusPlaceholder, setInputType, setRegistrationData, setNextStepRegistration} = authorizationSlice.actions;
+    const {registrationStep} = useAppSelector(state => state.authorizationReducer);
+    const {setNextStepRegistration} = authorizationSlice.actions;
     const dispatch = useAppDispatch();
 
     const {
         register,
         handleSubmit,
         reset,
-        formState: { errors, isValid },
-        getValues,
-    } = useForm<Inputs>({
+        formState: { errors, isValid }
+    } = useForm<IRegistrationData>({
         mode: 'onChange'
     })
-
+    console.log(errors);
     const onSubmit: SubmitHandler<Inputs> = (data) => {
         console.log(data);
-        console.log(getValues('password'));
-        console.log(getValues('login'));
         reset();
         dispatch(setNextStepRegistration());
     }
@@ -49,43 +42,16 @@ export const Registration = () => {
                                inputRegistrationData={'login'}
                                needValidate={true}
                                inputNumber={'First'}
-                               inputTypeProp={'text'}/>
-                <div className={styles.wrapperInput}>
-                    <p className={(isOnFocusSecondPlaceholder || registrationData.password) ? styles.onFocusPlaceholder : styles.notFocusPlaceholder}>Пароль</p>
-                    <input className={(errors.password?.message) ? styles.inputWrong  : styles.inputValid} type={inputType}
-                           onFocus={() => dispatch(setOnFocusPlaceholder('Second'))}
-                           {...register("password",
-                               {
-                                   onBlur: () => dispatch(setOnFocusPlaceholder('Second')),
-                                   onChange:(e) => dispatch(setRegistrationData({type: 'password', value: e.target.value})),
-                                   required: true,
-                                   validate: {
-                                       checkPassword: (value) => {
-                                           const hasMinLength = /^.{8,}$/.test(value);
-                                           const hasUppercaseChar = /[A-Z]/.test(value);
-                                           const hasAllParams = /(?=.*[A-ZА-Яa-zа-я\d])(?=.*[A-ZА-Я]).{8,}/.test(value);
-                                           const hasNumbersOnly = /\d/.test(value);
-                                           if (hasAllParams) {
-                                               return true;
-                                           } else if (hasMinLength && hasNumbersOnly && !hasUppercaseChar) {
-                                               return 'с заглавной буквой';
-                                           } else if (hasNumbersOnly && !hasUppercaseChar && !hasMinLength) {
-                                               return 'не менее 8 символов';
-                                           } else {
-                                               return 'Пароль не менее 8 символов, с заглавной буквой и цифрой';
-                                           }
-                                       },
-                                   },
-                               })}/>
-                    {(registrationData.password) ? <div className={styles.wrapperIcon}>
-                            {(errors.hasOwnProperty('password')) ? null : <IconCheck/>}
-                        {(inputType === 'password') ? <IconEye onClick={() => dispatch(setInputType('text'))}/>
-                            : <IconEyeClosed onClick={() => dispatch(setInputType('password'))}/>}
-                    </div> : null}
-                </div>
-                <p className={styles.inputTooltip}>{(errors.password?.message)
-                    ? highlightedText('password',errors.password.message)
-                    : 'Пароль не менее 8 символов, с заглавной буквой и цифрой'}</p>
+                               inputTypeProp={'text'}
+                               register={register}
+                               errors={errors}/>
+                    <AuthInput placeholderValue={'Пароль'}
+                               inputRegistrationData={'password'}
+                               needValidate={true}
+                               inputNumber={'Second'}
+                               inputTypeProp={'password'}
+                               register={register}
+                               errors={errors}/>
                     <MyButton content={'Следующий шаг'}
                               size={'btnLg'}
                               margin={'25px 0 0 0'}
